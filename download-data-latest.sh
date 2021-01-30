bf9ff0d315b9b7f2a15c75b6dffbd22e1c3f925a
GollyTicker <golly.ticker@gmail.com>
2021-01-30 22:17:56 +0000

    cleanup

--- download-data-latest.sh
+++ download-data-latest.sh
@@ -4,15 +4,8 @@ set -e
 # duplicated in other scripts
 docker build -t tagesschau:v1 .
 
-CMD="
-"
-
-rm -rf tmp && mkdir -p tmp
-echo "$CMD" > tmp/run.sh
-
 # duplicated in other scripts
 docker run --rm \
   --name tagesschau-downloader \
   -v ${PWD}/data:/app/data \
   tagesschau:v1 /bin/sh src/internal-download-data.sh
-rm -rf tmp
