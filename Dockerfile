67f7ada75e6016af9d84427b09770a85c1b4c841
GollyTicker <golly.ticker@gmail.com>
2021-04-04 13:57:37 +0200

    Added debug mode for docker and shell script. Added python interpreter to IDE.

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,4 @@ COPY requirements.txt ./
 RUN pip3 install -r requirements.txt
 COPY config config
 COPY src src
-CMD python3  src/2-http.py
+CMD python3 src/2-http.py
--- restart-http-service.sh
+++ restart-http-service.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+# Call this script with --dev option to run docker container directly.
+
 ./stop-http-service.sh || true
 
 PORT=`cat config/config.yml | grep "http-listen-port" | awk '{print $2}'`
@@ -9,12 +11,19 @@ PORT=`cat config/config.yml | grep "http-listen-port" | awk '{print $2}'`
 docker build -t tagesschau:v1 .
 
 # duplicated in other scripts
-CMD="docker run --rm \
---name tagesschau-runner \
+CMD0="docker run --rm"
+CMD1="--name tagesschau-runner \
 -v ${PWD}/data:/app/data \
 -v ${PWD}/config:/app/config \
 -p ${PORT}:${PORT} tagesschau:v1"
 
-echo "$CMD"
-
-(nohup $CMD 2>&1 &) > /dev/null
+if [[ "$1" == "--dev" ]]; then
+  # interactive unbuffered console during debugging
+  CMD="$CMD0 -it $CMD1 python3 -u src/2-http.py --dev"
+  echo $CMD
+  $CMD
+else
+  CMD="$CMD0 $CMD1"
+  echo $CMD
+  (nohup $CMD 2>&1 &) > /dev/null
+fi
