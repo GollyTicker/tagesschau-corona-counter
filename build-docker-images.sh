be963f11de7ced66725ee8d563517ecde787d471
GollyTicker <golly.ticker@gmail.com>
2022-07-28 18:34:16 +0200

    Use auto-updating python:slim.

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -5,10 +5,10 @@ ENV="$1"
 
 source config/source.sh
 
-docker build -f docker/Dockerfile -t tagesschau:v1 .
-docker build -f docker/Dockerfile.regular-download -t tagesschau-reloader:v1 .
+docker build --pull -f docker/Dockerfile -t tagesschau:v1 .
+docker build --pull -f docker/Dockerfile.regular-download -t tagesschau-reloader:v1 .
 
 if [[ "$ENV" == "dev" ]]; then
-  docker build -f docker/Dockerfile.web --target builder -t tagesschau-web:dev .
+  docker build --pull -f docker/Dockerfile.web --target builder -t tagesschau-web:dev .
 fi
-docker build -f docker/Dockerfile.web -t tagesschau-web:v1 .
+docker build --pull -f docker/Dockerfile.web -t tagesschau-web:v1 .
