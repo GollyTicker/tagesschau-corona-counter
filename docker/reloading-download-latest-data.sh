afcc8c8459979bed3cac58a0808c0e11d2d87028
GollyTicker <golly.ticker@gmail.com>
2023-05-05 23:34:19 +0200

    THE BET IS OVER: 🥳 21.03.2023 🥳

--- docker/reloading-download-latest-data.sh
+++ docker/reloading-download-latest-data.sh
@@ -4,13 +4,11 @@
 echo "============ STARTING DOWNLOAD OF LATEST DATA =========== $(date)"
 
 docker run --rm \
- --name tagesschau-downloader \
- -v tagesschau-data:/app/data \
- tagesschau:v1 /bin/sh src/internal-download-data.sh
+  --name tagesschau-downloader \
+  -v tagesschau-data:/app/data \
+  tagesschau:v1 /bin/sh src/internal-download-data.sh
 
 echo "============ FINISHED DOWNLOAD OF LATEST DATA =========== $(date)"
-
-
 
 echo "============ RESTARTING HTTP SERVICE ============ $(date)"
 
